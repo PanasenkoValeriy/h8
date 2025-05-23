6df69389a7f8095817aad5141461e1518ec9531a
PanasenkoValeriy <124078834+PanasenkoValeriy@users.noreply.github.com>
2025-05-23 12:38:47 +0200

    up

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+const { Component } = require('react');
+const { Feedback } = require('./Feedback/Feedback');
+const { Statistics } = require('./Statistics/Statistics');
+const { Section } = require('./Section/Section');
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return total === 0 ? 0 : Math.round((this.state.good / total) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <Feedback onLeaveFeedback={this.handleFeedback} />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,9 @@
+export const Feedback = ({ onLeaveFeedback }) => {
+  return (
+    <div>
+      <button onClick={() => onLeaveFeedback('good')}>Good</button>
+      <button onClick={() => onLeaveFeedback('neutral')}>Neutral</button>
+      <button onClick={() => onLeaveFeedback('bad')}>Bad</button>
+    </div>
+  );
+};
